43f84bbf73d07633405d7b984f63eddca603a372
BomberFish <bomberfish@bomberfish.ca>
2023-12-18 20:12:46 -0500

    Initial Commit

---
+++ CAPerfHudSwift/CAPerfHudSwiftApp.swift
@@ -0,0 +1,14 @@
+// bomberfish
+// CAPerfHudSwiftApp.swift – CAPerfHudSwift
+// created on 2023-12-18
+
+import SwiftUI
+
+@main
+struct CAPerfHudSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
